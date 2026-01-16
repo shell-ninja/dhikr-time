a49a2c3619e1e97a957e0ac66c2e8acc7f377283
shell-ninja <me.js.bro@gmail.com>
2026-01-16 08:46:32 +0600

    header section

--- src/Shared/Header/Header.jsx
+++ src/Shared/Header/Header.jsx
@@ -2,22 +2,29 @@ import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <div className="bg-cyan-600 flex justify-between">
-      {/* Logo */}
-      <div>
-        <Link to="/">Dhikr Time</Link>
-      </div>
+    <>
+      {/* Mobile Device */}
 
-      {/* Nav Links */}
-      <div>
-        <Link className="" to="/dua">
-          Dua
-        </Link>
-        <Link className="" to="/asma-ul-husna">
-          Asma Ul Husna
-        </Link>
+      <div></div>
+
+      {/* Others */}
+      <div className="bg-[#105A59] py-7 hidden md:flex justify-around font-amiri font-bold text-3xl text-[#E4F6D9] tracking-wide sticky top-0 z-[100]">
+        {/* Logo */}
+        <div>
+          <Link to="/">Dhikr Time</Link>
+        </div>
+
+        {/* Nav Links */}
+        <div className="">
+          <Link className="mr-10" to="/dua">
+            Dua
+          </Link>
+          <Link className="" to="/asma-ul-husna">
+            Asma Ul Husna
+          </Link>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
