6ecd772419cb7f7a3c655f2202da334c3ceabe13
shell-ninja <me.js.bro@gmail.com>
2026-02-09 21:10:14 +0600

    slightly updated.

--- src/Components/Dua/Dua.jsx
+++ src/Components/Dua/Dua.jsx
@@ -9,9 +9,13 @@ const Dua = () => {
         <h1 className="text-5xl font-amiri font-bold text-[#105A59]">Dua</h1>
 
         <div className="h-2 w-[75%] md:w-[40%] bg-[#105A59] rounded-2xl mt-2 mb-8"></div>
-        <div className="flex flex-col justify-center items-center font-amiri text-2xl md:text-3xl text-[#105A59]">
+        <div className="flex flex-col justify-center items-center text-start font-amiri text-2xl md:text-3xl text-[#105A59]">
           <p>This page is under development</p>
-          <p>The developer will update this page soon In-Sha-Allah</p>
+          <p>
+            The Developer is preparing the API for
+            <strong> 'Authentic Dua and Dhikrs'</strong>.
+          </p>
+          <p>This page will be updated soon In-Sha-Allah</p>
         </div>
       </div>
     </PageTransition>
